ad630112d62f1a5d64699bbaa2072c2844336976
Jus de Patate_ <jusdepatate@protonmail.com>
2020-03-04 04:21:43 +0100

    Tiny monitoring bot
    - /status -> gives ping, ram, cpu and disk usage
    - /cpu -> gives CPU usage per cores
    - /disk -> gives disk usage per partitions

---
+++ bot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# Jus de Patate_ - Telegram Monitoring Bot (github:jusdepatate/telegram-monitoring-bot) - 2020
+# Telegram bot that gives informations on server
+
+import telebot
+import psutil
+import time
+
+TOKEN = ""
+
+bot = telebot.TeleBot(TOKEN)
+# login
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "If you don't know what this bot does, it's maybe not made for you")
+	# eheh
+
+
+@bot.message_handler(commands=['status'])
+def send_welcome(message):
+	RAM = str(round(getattr(psutil.virtual_memory(), 'used') / getattr(psutil.virtual_memory(), 'total') * 100))
+	CPU = str(round(psutil.cpu_percent(interval=None, percpu=False) * 10))
+	DISK = str(round(psutil.disk_usage(psutil.disk_partitions()[0].mountpoint).percent))
+
+	sent = time.monotonic()
+	msg = bot.reply_to(message, "Ping-pong!")
+	ping = (time.monotonic() - sent) * 1000
+	ping = int(ping)
+
+	bot.edit_message_text(
+		"Ping: " + str(ping) + "ms\n"
+		"RAM: " + RAM + "%\n"
+		"CPU: " + CPU + "% (/cpu for more)\n"
+		"Disk: " + DISK + "% (/disk for more)",
+		message_id=msg.message_id, chat_id=msg.chat.id)
+	# here we send a message, then edit it to get """"ping""""
+	# we also give RAM/CPU/Disk (first partition) usage from psutil
+
+
+@bot.message_handler(commands=['cpu'])
+def cpu_usage(message):
+	i=0
+	msg=""
+	while True:
+		try:
+			cpup = psutil.cpu_percent(percpu=True)
+
+			msg += "CPU "+str(i+1)+": "+str(cpup[i] * 10)+"\n"
+			i+=1
+		except IndexError:
+			break
+	bot.reply_to(message, msg)
+	# Gives CPU usage per cores
+
+
+@bot.message_handler(commands=['disk'])
+def disk_usage(message):
+	partitions = psutil.disk_partitions()
+	format = "{}\t{}\t{}\t{}%\n"
+	msg = format.format("Disk", "Fs", "Mountpoint", "Av.Space in ")
+	# Only show a couple of different types of devices, for brevity.
+	i=0
+	while True:
+		try:
+			partition = partitions[i]
+			if partition.fstype == "squashfs":
+				i+=1
+				# ignore squashfs
+			else:
+				msg += format.format(partition.device, partition.fstype, partition.mountpoint, psutil.disk_usage(partition.mountpoint).percent)
+				# add drive (usually starts by '/dev/'), filesystem, mountpoint and disk usage in %
+				i+=1
+		except IndexError:
+			break
+	# i know you hate that way to do <3
+	bot.reply_to(message, msg)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, "Unknown command")
+
+
+bot.polling()
